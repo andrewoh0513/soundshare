f9b61868da3639b1c556fb1b6a828e7fdaedbf31
andrewoh0513 <143766112+andrewoh0513@users.noreply.github.com>
2024-04-20 22:26:06 +0000

    supabaseauth

--- src/App.js
+++ src/App.js
@@ -1,56 +1,38 @@
-import { useState, useEffect } from 'react';
 import './App.css';
-import { Container, Form, Button, Row, Col, Card } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { createClient } from '@supabase/supabase-js';
+import { useEffect, useState } from "react";
+import { Auth } from '@supabase/auth-ui-react';
 import { v4 as uuidv4 } from 'uuid';
+import { Button } from "@material-tailwind/react";
+import Spotifycont from './components/spotifycont';
 
-const SBCDNURL = "https://jcsfmlmjvoopwbgjtraq.supabase.co/storage/v1/object/public/sounds/"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=b45aa840d24945f296114fabf2cf3cf9&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_ANON_KEY
+);
+
+document.body.style = 'background: #f5f5f5;';
 
 function App() {
-  const [ email, setEmail ] = useState("");
-  const [ sounds, setSounds ] = useState([]);
-  const user = useUser();
-  const supabase = useSupabaseClient();
 
-  async function getSounds() {
-    const { data, error } = await supabase
-      .storage
-      .from('sounds')
-      .list(user?.id + "/", {
-        limit: 10,
-        offset: 0,
-        sortBy: { column: "name", order: "asc"}
-      });
-
-      if(data !== null) {
-        setSounds(data);
+  const [media, setMedia] = useState([]);
+  const [userId, setUserId] = useState('');
+  const [user, setUser] = useState(null);
+  
+  const getUser = async () => {
+    try {
+      const { data: { user } } = await supabase.auth.getUser();
+      if (user !== null) {
+        setUserId(user.id);
       } else {
-        alert("Error loading sounds");
-        console.log(error);
+        setUserId('');
       }
-  }
-
-  useEffect(() => {
-    if(user) {
-      getSounds();
+    } catch (e) {
+      console.error('Error getting user:', e.message);
     }
-  }, [user]);
-
-  async function LinkLogin() {
-    const { data, error } = await supabase.auth.signInWithOtp({
-      email: email
-    });
-    if(error) {
-      alert("Error communicating with supabase, make sure to use a real email address.");
-    } else {
-      alert("Check your email for a Link to log in!");
-    }
-  }
-
-  async function signOut() {
-    const { error } = await supabase.auth.signOut();
-  }
+  };
 
   async function uploadSound(e) {
     let file = e.target.files[0];
@@ -58,81 +40,123 @@ function App() {
     const { data, error } = await supabase
       .storage
       .from('sounds')
-      .upload(user.id + "/" + uuidv4(), file)
+      .upload(uuidv4(), file);
 
-    if(data) {
-      getSounds();
+    if (data) {
+      console.log('File uploaded successfully:', data);
+      getMedia();
     } else {
-      console.log(error);
+      console.error('Error uploading file:', error);
     }
   }
 
-  async function deleteSound(soundName) {
-    const { error } = await supabase
-      .storage
-      .from('sounds')
-      .remove([ user.id + "/" + soundName])
-    
-    if(error) {
-      alert(error);
+  async function getMedia() {
+    const { data, error } = await supabase.storage.from('sounds').list('', {
+      limit: 10,
+      offset: 0,
+      sortBy: {
+        column: 'name', order: 'asc'
+      }
+    });
+
+    if (data) {
+      setMedia(data);
     } else {
-      getSounds();
+      console.error('Error getting media:', error);
     }
   }
+
+  const code = new URLSearchParams(window.location.search).get('code');
+
+  const signout = async () => {
+    setUserId('');
+    await supabase.auth.signOut();
+  };
+
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        setUser(session?.user ?? null);
+        setUserId(session?.user?.id || '');
+      }
+    );
+    getUser();
+    getMedia();
+  }, [userId]);
 
   return (
-    <Container align="center" className='container-sm mt-4'>
-      {
-
-      }
-      { user === null ?
+    <>
+      {!user && (
+        <div className='text'>
+          <h2 className='text-center absolute w-full text-3xl font-semibold'>Get into Public Noise</h2>
+          <div className='inputlabel'>
+            <p className='email'>Email Address</p>
+            <p className='password'>Password</p>
+          </div>
+        </div>
+      )}
+      {userId === '' ? 
+        <Auth
+          supabaseClient={supabase}
+          localization={{
+            variables: {
+              forgotten_password: {
+                email_label: '',
+                password_label: '',
+                email_input_placeholder: '',
+                password_input_placeholder: '',
+              },
+              sign_up: {
+                email_label: '',
+                password_label: '',
+                email_input_placeholder: '',
+                password_input_placeholder: '',
+              },
+              sign_in: {
+                email_label: '',
+                password_label: '',
+                email_input_placeholder: '',
+                password_input_placeholder: '',
+              },
+            },
+          }}
+          appearance={{
+            extend: false,
+            className: {
+              anchor: 'anchor',
+              button: 'btn',
+              container: 'container',
+              divider: 'divider',
+              label: 'label',
+              input: 'input',
+              loader: 'loader',
+              message: 'message',
+            },
+          }}
+          providers={[]}
+        /> 
+        : 
         <>
-          <h1>loginpage</h1>
-          <Form>
-            <Form.Group className='mb-3' style={{maxWidth: "500px"}}>
-              <Form.Label>Enter Email to get started</Form.Label>
-              <Form.Control
-                type='email'
-                placeholder='Enter Email'
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-            <Button variant='primary' onClick={() => LinkLogin()}>
-              Get link
-            </Button>
-          </Form>
-        </>
-
-      :
-        <>
-          <h1>Loggedin</h1>
-          <Button onClick={() => signOut()}>Sign Out</Button>
-          <p>{user.email}</p>
-          <p>Use the Choose File button below to upload an sound to your gallery</p>
-          <Form.Group className="mb-3" style={{maxWidth: "500px"}}>
-            <Form.Control type="file" accept=".mp3,audio/*" onChange={(e) => uploadSound(e)}/>
-          </Form.Group>
-          <hr />
-          <h3>Your sounds</h3>
-          <Row xs={1} md={3} className="g-4">
-            {sounds.map((sound) => {
-              return (
-                <Col key={SBCDNURL + user.id + "/" + sound.name}>
-                  <Card>
-                    <audio controls>
-                      <source src={SBCDNURL + user.id + "/" + sound.name}/>
-                    </audio>
-                    <Card.Body>
-                      <Button variant="danger" onClick={() => deleteSound(sound.name)}>Delete sound</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              )
-            })}
-          </Row>
+          <input type="file" onChange={(e) => uploadSound(e)} />
+          <div className='mt-5'>
+            This is Public Noise
+          </div>
+          {media.map((mediaItem) => (
+            <div key={mediaItem.name}>
+              <audio controls>
+                <source src={`https://odzavsveqyollnpomckm.supabase.co/storage/v1/object/public/sounds/${mediaItem.name}`} />
+              </audio>
+            </div>
+          ))}
+          <div className='mt-5'>
+            <button className='bg-gray-300' onClick={signout}>
+              Logout
+            </button>
+          </div>
+          <Spotifycont />
         </>
       }
-    </Container>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  "https://jcsfmlmjvoopwbgjtraq.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impjc2ZtbG1qdm9vcHdiZ2p0cmFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMzI3ODYsImV4cCI6MjAyODkwODc4Nn0.VlvMWfnJjSsGV8n-r-c8VUEpU-D3rz3fEvlnHKa-_VQ",
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SessionContextProvider supabaseClient={supabase}>
     <App />
-    </SessionContextProvider>
   </React.StrictMode>
 );
 
